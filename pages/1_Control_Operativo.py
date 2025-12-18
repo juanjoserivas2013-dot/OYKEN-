f140892932ca86190753a1ce8bd1864fffaca2a0
juanjoserivas2013-dot <juanjoserivas2013@gmail.com>
2025-12-18 01:00:51 +0100

    Create 1_Control_Operativo.py

---
+++ pages/1_Control_Operativo.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.title("Control Operativo")
+st.caption("Gestión diaria y control operativo")
+
+st.markdown("""
+Aquí irá:
+- Bloque HOY
+- Bloque DOW
+- Variaciones
+- Bitácora
+""")

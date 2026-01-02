fced4feec078a47d74889a4fdc6a867cf861b448
juanjoserivas2013-dot <juanjoserivas2013@gmail.com>
2026-01-02 14:42:25 +0100

    Update app.py

--- app.py
+++ app.py
@@ -9,12 +9,3 @@ st.set_page_config(
 st.title("OYKEN")
 st.caption("Consultoría creadora de Soluciones Generativas, apoyada en infraestructura cognitiva Propia"
 )
-
-st.markdown("""
-OYKEN genera lectura operativa, escenarios de decisión y alerta inteligente adaptadas a cada negocio,
-a partir de su realidad diaría.
-
-No sustituye sistemas ya existentes: Los interpreta y los convierte en criterio accionable.
-""")
-           
-st.markdown("Selecciona un módulo en el menú lateral")
